46524e11d251c983ff8a95a81fad6b75ac665dd8
Harsh <71979122+CodyHarsh@users.noreply.github.com>
2023-03-03 11:15:20 +0530

    Time: 5 ms (28.46%), Space: 7.6 MB (83.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -5,6 +5,9 @@ public:
         
         //Priority queue is queue data structure for implementation
         //Heap uses tree structure and in sorting way
+        
+        
+        //priority queue insertion time is logn for n it is nlogn
         priority_queue<int> ans;
         for(auto i: stones){
             ans.push(i);
@@ -20,10 +23,14 @@ public:
                  ans.push(a-y);
              }
         }
+        
         if(ans.empty()){
             return 0;
         }
-        return ans.top();
         
+        
+        return ans.top();
+        //Time Complexity: O(nlogn)
+        //Space Complexity: O(n)
     }
 };
