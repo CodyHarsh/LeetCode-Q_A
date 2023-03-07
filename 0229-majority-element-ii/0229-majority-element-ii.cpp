bedf92788e89206a3079e1c3d888f1d68791f2c8
Harsh <71979122+CodyHarsh@users.noreply.github.com>
2023-03-07 19:03:35 +0530

    Time: 19 ms (31.49%), Space: 16 MB (60.15%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
+
+        if(nums.size() == 1){
+            ans.push_back(nums[0]);
+            return ans;
+        }
+        int num1 = -1;
+        int num2 = -1;
+        int count1 = 0;
+        int count2 = 0;
+        
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == num1){
+                count1++;
+            }else if(nums[i] == num2){
+                count2++;
+            }else if(count1 == 0){
+                num1 = nums[i];
+                count1 =1;
+            }else if(count2 == 0){
+                num2 = nums[i];
+                count2= 1;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        for(int i= 0; i<nums.size(); i++){
+            if(num1 == nums[i]){
+                count1++;
+            }
+            if(num2 == nums[i]){
+                count2++;
+            }
+        }
+        
+        int nDivideBy3 = (nums.size()/3);
+        if(num1 == num2){
+            ans.push_back(num1);
+            return ans;
+        }
+        
+        if(count1 > nDivideBy3){
+            ans.push_back(num1);
+        }
+        if(count2 > nDivideBy3){
+            ans.push_back(num2);
+        }
+        return ans;
+    }
+};
