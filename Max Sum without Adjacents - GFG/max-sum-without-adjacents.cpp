2436f7bb5345dd93b1cfeabc669d6e5d0e90ef53
Harsh <71979122+CodyHarsh@users.noreply.github.com>
2023-07-07 15:28:41 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -10,26 +10,46 @@ class Solution{
 public:	
 	// calculate the maximum sum with out adjacent
 	int findMax(int *arr, int n, vector<int> &memoization){
-	    if(n <= 0){
-	        return 0;
-	    }
-	    if(n == 1){
-	        return arr[0];
-	    }
-	    if(memoization[n-1] != -1){
-	        return memoization[n-1];
-	    }
-	    //include 
-	    int x =arr[n-1] +  findMax(arr, n - 2, memoization);
-	    //Don't include 
-	    int y = findMax(arr, n-1, memoization);
-	    return(memoization[n-1] = max(x,y));
+	   // if(n <= 0){
+	   //     return 0;
+	   // }
+	   // if(n == 1){
+	   //     return arr[0];
+	   // }
+	   // if(memoization[n-1] != -1){
+	   //     return memoization[n-1];
+	   // }
+	   // //include 
+	   // int x =arr[n-1] +  findMax(arr, n - 2, memoization);
+	   // //Don't include 
+	   // int y = findMax(arr, n-1, memoization);
+	   // return(memoization[n-1] = max(x,y));
 	}
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    vector<int> memoization (n, -1);
-	    return findMax(arr, n, memoization);
+	    vector<int> dp (n, 0);
+	   // dp[0] = n-1; 
+	   // for(int i = 1; i>n; i++){
+	   //     int pick = INT_MAX;
+	   //     if(i > 1)
+	   //     pick  = arr[i] + dp[i-2];
+	   //     int nonPick  = dp[i-1]
+	        
+	   //     dp[i-1]
+	   // }
 	    
+	   // return findMax(arr, n, memoization);
+	    
+	    for(int i=0; i<n; i++){
+	        dp[i] = max(
+	            ((i -1 >= 0) ? dp[i-1] : arr[i]), 
+	            ((i-2 >= 0) ? arr[i] + dp[i-2] : arr[i])
+	            );
+	           //cout << dp[i] << " ";
+	    }
+	    
+	    
+	    return dp[n-1];
 	}
 };
 
