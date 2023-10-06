6cba593cf2224a7a34556727b8a130ef7e84d373
Harsh <71979122+CodyHarsh@users.noreply.github.com>
2023-10-06 18:30:26 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -7,40 +7,55 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
-	
-    void topo(int V, vector<int> adj[], stack<int> &temp, vector<int> &visited, int curr){
-        
-        
-        for(auto i: adj[curr]){
-            if(!visited[i]){
-                visited[i] = 1;
-                topo(V, adj, temp, visited, i);
-            }
-        }
-        
-        temp.push(curr);
-    }
-    
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+	    //Using BFS or Kahn's algoirthm:
 	    
-	    stack<int> temp;
-	    vector<int> visited(V, 0);
+	    //Making the indegree array:
+	    vector<int> indegree(V, 0);
 	    
-	    for(int i = 0; i<V; i++){
-	        if(!visited[i]){
-	            visited[i] = 1;
-	            topo(V, adj, temp, visited, i);
+	    for(int i =0; i<V; i++){
+	        for(auto j: adj[i]){
+	            indegree[j]++;
 	        }
 	    }
-	    vector<int> ans;
-	    while(!temp.empty()){
-	        ans.push_back(temp.top());
-	        temp.pop();
+	    
+	    
+	    queue<int> q;
+	    //We have to find the 0's in indegree and add it into the queue
+	    for(int i = 0; i<V; i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
 	    }
 	    
+	    vector<int> ans;
+	    
+	    //int n  = 10; 1 to 10: for(auto: )
+	    
+	    //Now traverse it and find the 0's till queue is empty:
+	    while(!q.empty()){
+	        int temp  = q.front();
+	        q.pop();
+	        ans.push_back(temp);
+	        //Now check for the 0's in thed company:
+	        
+	        for(auto i: adj[temp]){
+	            indegree[i]--;
+	             if(indegree[i] == 0){
+	                  //Now check if anyone have become 0 or not:
+	                  q.push(i);
+	             }
+	        }
+	        
+	        
+	       
+	    }
+	    
+	   
 	    return ans;
+	    
 	    
 	}
 };
